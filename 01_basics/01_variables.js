6d29c04823d7b3ba2536d3099dab9837edd2af9b
mishbahul-hub <lm8115801@gmail.com>
2026-03-16 22:47:48 +0530

    Day 01

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const account_id = 16735
+let account_email = "mishbahul@google.com"
+var account_password = "82364"
+account_city = "Jaipur"
+let account_state;
+
+// account_id = 2 // not allowed
+
+account_email = "hdfh@jhfd.com"
+account_password = "hdfh346"
+account_city = "Bangalore"
+
+/*
+prefer not to use var
+because if issue in block scope and functional scope
+*/
+
+console.table([account_id, account_email,  account_password, account_city, account_state])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js , not browser
+
+console.log(3 + 3)
+
+// for documentation use ECMAScripts
+// {'https://ecma-international.org/publications-and-standards/standards/ecma-262/'}
+
+let name = "mishbahul"
+let age = 20
+let isLoggedIN = false // boolean
+let state;
+
+// number -> 2 to power 53
+// bigint
+// string -> ""
+// boolean -> true or false
+// null -> standalone value
+// undefined -> 
+// symbol -> for uniqueness
+
+// object 
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
