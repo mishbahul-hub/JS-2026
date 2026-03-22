22828a26c9cd0d8b3f156ed98b27765678cf6b7b
mishbahul-hub <lm8115801@gmail.com>
2026-03-22 21:07:52 +0530

    Day-06

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,22 @@
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myarray = ['flash', "batman", "superman"]
+
+let arr = 0
+while(arr < myarray.length){
+    // console.log(`Value is ${myarray[arr]}`)
+    arr = arr + 1
+}
+
+let score = 0
+do {
+    console.log(`Score is ${score}`)
+    score++
+
+} while(score <= 10)
+
+    // this first does the task then checks the condition
